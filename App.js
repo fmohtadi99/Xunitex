f310712de2e05e419712b148870c7b55ec744ae2
fmohtadi99 <fmohtadi99@gmail.com>
2022-03-10 00:14:04 +0330

    App modified

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import {View, Text} from 'react-native';
+import Auth from './Components/Auth';
+
+class App extends Component{
+    render(){
+        return(
+            <Auth/>
+        )
+    }
+}
+
+export default App;
---
+++ Components/Auth/Auth.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Button} from '../Material';
+
+const Auth = () => {
+    return(
+        <Button>Login</Button>
+    )
+}
+
+export default Auth;
