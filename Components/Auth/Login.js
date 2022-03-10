7c0eb1717b9a196dd0426246689e2ec8c3e6988b
fmohtadi99 <fmohtadi99@gmail.com>
2022-03-10 10:48:43 +0330

    Update Login.js

--- Components/Auth/Login.js
+++ Components/Auth/Login.js
@@ -1,19 +1,40 @@
 import React, { Component } from "react";
 import { View, Text } from 'react-native';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import { Button, TextInput } from '../Material';
+import { Button, TextInput, Snackbar } from '../Material';
 import { Settings, Themes } from '../../Resources/index';
 
 let CurrentTheme = Settings.CurrentTheme;
 
 class Login extends Component {
 
-    state = { email: '', password: '' };
+    state = { email: '', password: '', passwordError: false, emailError: false, errorMessage: '', error: false, loading: false };
 
     onLoginPress() {
+        this.setState({ loading: true });
         const { email, password } = this.state;
-        signInWithEmailAndPassword(getAuth, email, password)
-             .catch(()=>{alert('Wrong Information')});
+        signInWithEmailAndPassword(getAuth(), email, password)
+            .catch((error) => {
+                this.setState({ error: true });
+                switch (error.code) {
+                    case "auth/wrong-email":
+                        this.setState({ passwordError: true, errorMessage: 'Invalid Email. Try again!' });
+                        break;
+                    case "auth/wrong-password":
+                        this.setState({ passwordError: true, errorMessage: 'Invalid Password. Try again!' });
+                        break;
+                }
+
+            })
+            .then(() => this.setState({ loading: false }));
+    }
+
+    clearError() {
+        this.setState({ passwordError: false, emailError: false, errorMessage: '' });
+    }
+
+    needHelp() {
+
     }
 
     render() {
@@ -36,33 +57,41 @@ class Login extends Component {
                 </View>
 
                 <View style={Styles.SectionInput}>
-                    <TextInput label="Email" onChangeText={email => this.setState({ email })} placeholder="user@domain.com" UnderlineColor="transparent" style={{
+                    <TextInput label="Email" value={this.state.email} error={this.state.emailError} onChangeText={email => { this.setState({ email }); this.clearError() }} placeholder="user@domain.com" UnderlineColor="transparent" style={{
                         width: "100%",
                         textAlign: 'center',
                         fontFamily: Themes.Fonts[CurrentTheme].Regular,
                         fontSize: Themes.Fonts[CurrentTheme].SizeBold,
                         marginBottom: 8,
                     }} />
-                    <TextInput label="Password" secureTextEntry onChangeText={password => this.setState({ password })} placeholder="••••" style={{
+                    <TextInput label="Password" value={this.state.password} error={this.state.passwordError} secureTextEntry onChangeText={password => { this.setState({ password }); this.clearError() }} placeholder="••••" style={{
                         width: "100%",
                         textAlign: 'center',
                         fontFamily: Themes.Fonts[CurrentTheme].Regular,
                         fontSize: Themes.Fonts[CurrentTheme].SizeBold,
                         marginBottom: 48
                     }} />
-                    <Button mode="contained" onPress={this.onLoginPress.bind(this)} labelStyle={{
+                    <Text style={{ marginBottom: 12 }}>{this.state.errorMessage}</Text>
+                    <Button loading={this.state.loading} mode="contained" onPress={this.onLoginPress.bind(this)} labelStyle={{
                         fontFamily: Themes.Fonts[CurrentTheme].Regular,
                         fontSize: Themes.Fonts[CurrentTheme].SizeRegular,
                         textTransform: 'capitalize'
                     }}
                         style={{ marginBottom: 8 }}>Login</Button>
-                    <Button onPress={() => { alert(this.state.email + this.state.password) }} labelStyle={{
+                    <Button onPress={this.needHelp()} labelStyle={{
+                        fontFamily: Themes.Fonts[CurrentTheme].Regular,
+                        fontSize: Themes.Fonts[CurrentTheme].SizeSmall,
+                        textTransform: 'capitalize'
+                    }}
+                        style={{ marginBottom: 8 }}>Need Help?</Button>
+                    <Button onPress={this.needHelp()} labelStyle={{
                         fontFamily: Themes.Fonts[CurrentTheme].Regular,
                         fontSize: Themes.Fonts[CurrentTheme].SizeSmall,
                         textTransform: 'capitalize'
                     }}
                         style={{ marginBottom: 8 }}>Need Help?</Button>
                 </View>
+                <Snackbar style={Styles.Snackbar} duration={5000} visible={this.state.error}>{this.state.errorMessage}</Snackbar>
             </View>
         )
     }
@@ -90,6 +119,10 @@ const Styles = {
         justifyContent: 'flex-start',
         alignItems: 'center',
         paddingHorizontal: 18
+    },
+    Snackbar: {
+        flex: 1,
+        justifyContent: 'space-between'
     }
 }
 
