5564872b1aa02e045856e954365e738569f576ae
fmohtadi99 <fmohtadi99@gmail.com>
2022-03-19 13:54:17 +0330

    Formal Arranged

--- Resources/index.js
+++
@@ -1,6 +0,0 @@
-import APIs from './APIs.json';
-import Icons from './Icons.json';
-import Settings from './Settings.json';
-import Themes from './Themes.json';
-
-export { APIs, Icons, Settings, Themes };
